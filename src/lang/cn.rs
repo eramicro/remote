2815c2fb7e7f8cda3b4c32b899da57aae3a19c9f
eramicro <645452175@qq.com>
2024-09-02 11:35:43 +0800

    cn.rs

--- src/lang/cn.rs
+++ src/lang/cn.rs
@@ -145,7 +145,7 @@ pub static ref T: std::collections::HashMap<&'static str, &'static str> =
         ("Failed to make direct connection to remote desktop", "无法直接连接到远程桌面"),
         ("Set Password", "设置密码"),
         ("OS Password", "操作系统密码"),
-        ("install_tip", "当前为绿色版，由于系统UAC限制，当某些功能无法使用时，请点击下面按钮进行安装。"),
+        ("install_tip", "精简版运行中，请点击下面按钮，安装使用完整功能。"),
         ("Click to upgrade", "点击这里升级"),
         ("Click to download", "点击这里下载"),
         ("Click to update", "点击这里更新"),
