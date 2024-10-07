f3d28a1ad7e7ed36aafee252cc53bb1edf9e3dc3
eramicro <645452175@qq.com>
2024-10-07 09:58:54 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["2025928183.icucloud.vip"];
-pub const PUBLIC_RS_PUB_KEY: &str = "";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["shengshen.cloud"];
+pub const PUBLIC_RS_PUB_KEY: &str = "pSlKrQWkhXhzQ+BbP30yz3bufQy20hQG8hH3Zshg6aA=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
