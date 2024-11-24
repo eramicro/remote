c20f46530504c96a51cd49e239f69e1904db68e2
eramicro <645452175@qq.com>
2024-11-24 10:28:32 +0800

    common.rs

--- src/common.rs
+++ src/common.rs
@@ -909,7 +909,7 @@ pub fn get_api_server(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "http://tg.163ll.com".to_owned()
+    "http://tg.163ll.com:21114".to_owned()
 }
 
 pub fn get_audit_server(api: String, custom: String, typ: String) -> String {
