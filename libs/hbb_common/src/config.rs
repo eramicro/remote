d32be0b41854b4b03235eaab2fbf0d3806382bd0
eramicro <645452175@qq.com>
2024-12-12 20:46:06 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -104,8 +104,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["218.21.35.151"];
-pub const PUBLIC_RS_PUB_KEY: &str = "ycgrgs6011852";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["218.21.35.156"];
+pub const PUBLIC_RS_PUB_KEY: &str = "ycgrgs60118526";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
